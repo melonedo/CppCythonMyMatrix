f535aa20612dccd3b6f6c39389893778da9716fb
melonedo <44501064+melonedo@users.noreply.github.com>
2024-01-14 12:43:22 +0800

    卷积测速代码

--- MyMatrix-bench.cpp
+++ MyMatrix-bench.cpp
@@ -1,15 +1,18 @@
 #include <nanobench.h>
+
 #include <chrono>
 
 #include "MyMatrix.hpp"
 
+using ankerl::nanobench::doNotOptimizeAway;
+
 int main() {
   const size_t size = 64;
-  double init[size][size];
+  float init[size][size];
   for (int i = 0; i < size; i++) {
     for (int j = 0; j < size; j++) init[i][j] = i;
   }
-  MyMatrix<double> mat(&init[0][0], size, size);
+  MyMatrix<float> mat64(&init[0][0], size, size);
 
   ankerl::nanobench::Bench b;
   b.relative(true);
@@ -17,10 +20,8 @@ int main() {
   b.warmup(100);
 
   b.title("MatMul 64x64");
-  b.run("matmul 64x64 slow",
-        [&] { ankerl::nanobench::doNotOptimizeAway(mat.mul(mat)); });
-  b.run("matmul 64x64 fast",
-        [&] { ankerl::nanobench::doNotOptimizeAway(mat * mat); });
+  b.run("matmul 64x64 slow", [&] { doNotOptimizeAway(mat64.mul(mat64)); });
+  b.run("matmul 64x64 fast", [&] { doNotOptimizeAway(mat64 * mat64); });
 
   float init55[5][5];
   for (int i = 0; i < 5; i++) {
@@ -29,10 +30,22 @@ int main() {
   MyMatrix<float> mat55(&init55[0][0], 5, 5);
 
   b.title("MatMul 5x5");
-  b.run("matmul 5x5 fast",
-        [&] { ankerl::nanobench::doNotOptimizeAway(mat55 * mat55); });
-  b.run("matmul 5x5 slow",
-        [&] { ankerl::nanobench::doNotOptimizeAway(mat55.mul(mat55)); });
-  b.run("matmul 5x5 simd",
-        [&] { ankerl::nanobench::doNotOptimizeAway(matmul55(mat55, mat55)); });
+  b.run("matmul 5x5 fast", [&] { doNotOptimizeAway(mat55 * mat55); });
+  b.run("matmul 5x5 slow", [&] { doNotOptimizeAway(mat55.mul(mat55)); });
+  b.run("matmul 5x5 simd", [&] { doNotOptimizeAway(matmul55(mat55, mat55)); });
+
+  MyMatrix<float> kernel{{0, -1, 0}, {-1, 2, -1}, {0, -1, 0}};
+  auto init512 = new float[512][512];
+  for (int i = 0; i < 512; i++) {
+    for (int j = 0; j < 512; j++) init512[i][j] = i;
+  }
+  MyMatrix<float> mat512(&init512[0][0], 512, 512);
+  b.title("Conv 64x64 with 3x3 kernel");
+  b.run("Conv 64x64 slow", [&] { doNotOptimizeAway(mat64.conv_slow(kernel)); });
+  b.run("Conv 64x64 fast", [&] { doNotOptimizeAway(mat64.conv(kernel)); });
+
+  b.title("Conv 512x512 with 3x3 kernel");
+  b.run("Conv 512x512 slow",
+        [&] { doNotOptimizeAway(mat512.conv_slow(kernel)); });
+  b.run("Conv 512x512 fast", [&] { doNotOptimizeAway(mat512.conv(kernel)); });
 }
--- test-python.ps1
+++ test-python.ps1
@@ -1,10 +1,10 @@
 echo Difference:
-python -c "from MyMatrixTests import *; print((A @ A - (B @ B).to_ndarray()).sum(), (A @ A - (B.mul(B)).to_ndarray()).sum(), (A @ A - matmul(A, A)).sum())"
+python -c "from MyMatrixTests import *; print(np.abs(A @ A - (B @ B).to_ndarray()).sum(), np.abs(A @ A - (B.mul(B)).to_ndarray()).sum(), np.abs(A @ A - matmul(A, A)).sum())"
 echo NumPy:
 python -m timeit -s "from MyMatrixTests import A" "A@A"
 echo Cpp-Accelerated:
 python -m timeit -s "from MyMatrixTests import B" "B@B"
 echo Cpp-Slow:
-python -m timeit -s "from MyMatrixTests import B" "B.slow_mul(B)"
+python -m timeit -s "from MyMatrixTests import B" "B.mul_slow(B)"
 echo Python:
 python -m timeit -s "from MyMatrixTests import A, matmul" "matmul(A, A)"
